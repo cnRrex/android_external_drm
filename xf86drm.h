eea87d02ba42c6851956d8f737096f0f42ee4b97
Emil Velikov <emil.l.velikov@gmail.com>
2015-03-07 00:58:39 +0000

    drm: add drmGet(Primary|Render)DeviceNameFromFd functions
    Currently most places assume reliable primary(master) <> render node
    mapping. Although this may work in some cases, it is not correct.
    
    Add a couple of helpers that hide the details and provide the name of
    the master or render device name, given an fd. The latter may belong to
    either the master, control or render node device.
    
    v2:
     - Rename Device and Primary to Master (aka the /dev/dri/cardX device).
     - Check for the file via readdir_r() rather than stat().
     - Wrap the check into a single function.
     - Return NULL for non-linux platforms.
    
    v3:
     - Don't segfault if name is NULL.
     - Update function names, as suggested by Frank Binns.
    
    v4:
     - Update commit message to reflect the function name changes.
    
    Cc: Frank Binns <frank.binns@imgtec.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: David Herrmann <dh.herrmann@googlemail.com>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Frank Binns <frank.binns@imgtec.com>

--- xf86drm.h
+++ xf86drm.h
@@ -774,6 +774,9 @@ extern int drmGetNodeTypeFromFd(int fd);
 extern int drmPrimeHandleToFD(int fd, uint32_t handle, uint32_t flags, int *prime_fd);
 extern int drmPrimeFDToHandle(int fd, int prime_fd, uint32_t *handle);
 
+extern char *drmGetPrimaryDeviceNameFromFd(int fd);
+extern char *drmGetRenderDeviceNameFromFd(int fd);
+
 extern int Calc_CSC_Param(struct CSCCoeff_Matrix *CSC_Matrix, struct csc_coeff *CSC_Coeff_t, int devid);
 
 #if defined(__cplusplus) || defined(c_plusplus)
